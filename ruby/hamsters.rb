5b93dc089d1c6b82b0f4241311f3eb7a1641df00
Kari Anne <kariag@icloud.com>
2016-07-15 13:58:27 -0400

    Add questions to find out hamsters name volume level fur color if its a good candidate for adoption and estimated age

---
+++ ruby/hamsters.rb
@@ -0,0 +1,14 @@
+puts "What is the hamsters name?"
+hamsters_name = gets.chomp
+
+puts "How loud is the hamster on a scale of 1 to 10?"
+volume_level = gets.chomp
+
+puts "What color fur does the hamster have?"
+fur_color = gets.chomp
+
+puts "Do you believe the hamster is a good candidate for adoption? Please answer yes or no."
+adopt_canditate = gets.chomp
+
+puts "What is the hamsters estimated age?"
+estimated_age = gets.chomp
