070064c81038362e09242019abad2d270a518b08
Kari Anne <kariag@icloud.com>
2016-07-15 14:00:37 -0400

    Updated code to convert volume level to and intger and candidate for adoption to a boolean

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,13 +2,13 @@ puts "What is the hamsters name?"
 hamsters_name = gets.chomp
 
 puts "How loud is the hamster on a scale of 1 to 10?"
-volume_level = gets.chomp
+volume_level = gets.chomp.to_i
 
 puts "What color fur does the hamster have?"
 fur_color = gets.chomp
 
 puts "Do you believe the hamster is a good candidate for adoption? Please answer yes or no."
-adopt_canditate = gets.chomp
+adopt_canditate = gets.chomp.to_b
 
 puts "What is the hamsters estimated age?"
 estimated_age = gets.chomp
