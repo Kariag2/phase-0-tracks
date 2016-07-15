bd8d213601c087f2ea9a88625b67df2969ad8ef6
Kari Anne <kariag@icloud.com>
2016-07-15 15:48:21 -0400

    Update program so it prints hamster data to the console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,14 +1,20 @@
 puts "What is the hamsters name?"
-hamsters_name = gets.chomp
+  hamsters_name = gets.chomp
 
 puts "How loud is the hamster on a scale of 1 to 10?"
-volume_level = gets.chomp.to_i
+  volume_level = gets.chomp.to_i
 
 puts "What color fur does the hamster have?"
-fur_color = gets.chomp
+  fur_color = gets.chomp
 
 puts "Do you believe the hamster is a good candidate for adoption? Please answer yes or no."
-adopt_canditate = gets.chomp.to_b
+  adopt_canditate = gets.chomp.to_b
 
 puts "What is the hamsters estimated age?"
-estimated_age = gets.chomp
+  estimated_age = nil
+
+puts "The hamsters name is #{hamsters_name}."
+puts "The hamster can get to a volume level of about #{volume_level}."
+puts "The hamsters fur color is #{fur_color}"
+puts "How we feel about #{hamsters_name} being a good candidate for adoption is #{adopt_canditate}."
+puts "Hamsters age is #{estimated_age}."
