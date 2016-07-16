956fb3ec80fede93a0c8c1665faa548dce4e2dd3
Kari Anne <kariag@icloud.com>
2016-07-15 22:21:32 -0400

    Add questions to gather informantion on new employees

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born"
+year_born = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread, should we order some for you?(yes/no)"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
