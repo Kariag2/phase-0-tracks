62e7aaf485985474c2d7bc3342e68b757f047523
Kari Anne <kariag@icloud.com>
2016-07-16 12:41:45 -0400

    Initial commit on new branch

---
+++ ruby/vampires.rb
@@ -0,0 +1,21 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born"
+year_born = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread, should we order some for you?(yes/no)"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
+
+current_year = 2016
+if current_year - year_born = age
+  age == "true"
+else
+  age == "false"
+end
