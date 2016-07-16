1ad7efae1147b5282b6f222ea0d333581c7cbaaa
Kari Anne <kariag@icloud.com>
2016-07-16 13:15:34 -0400

    Update code with correct age variable to equal true or false

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,16 +13,18 @@ garlic_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
 
-current_year = 2016
-if current_year - year_born = employee_age
-  employee_age == true
+current_year = 2016.to_i
+# If current year minus year born is equal to employee_age then correct_age is true
+if employee_age == current_year - year_born
+  correct_age = true
 else
-  employee_age == false
+  correct_age = false
 end
-p employee_age
+p correct_age
+# If garlic bread is equal to yes then garlic_bread will be true
 if garlic_bread == "yes"
-  garlic_bread == true
+  garlic_bread = true
 else
-  garlic_bread == false
+  garlic_bread = false
 end
-p garlic_bread
+#p garlic_bread
