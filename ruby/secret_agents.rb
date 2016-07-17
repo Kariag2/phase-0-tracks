e161d946c366949cff1faa19752d3d418befaa1d
Kari Anne <kariag@icloud.com>
2016-07-17 19:45:56 -0400

    Initial commit and add pseudocode of encrypt and decrypt methods

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,16 @@
+=begin
+Pseudocode - Encrypt method:
+Write an encrypt method that
+Advances every letter of a string one letter forward
+Assume lowercase input and output
+Any space characters should remain spaces
+=end
+
+
+
+=begin
+- Decrypt method:
+Write a decrypt method that
+Moves every letter of a string back one letter
+Spaces remain spaces
+=end
