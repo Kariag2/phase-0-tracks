8d14dc214876ac48798a4a78aa028a48fa67d1a1
Kari Anne <kariag@icloud.com>
2016-07-17 12:24:16 -0400

    Add employees processed question at the beginning and add a while loop to go through the employees processed

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,57 +1,62 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed today?"
+processed_today = gets.chomp.to_i
+employees_processed = 0.to_i
 
-puts "How old are you?"
-age = gets.chomp.to_i
+while employees_processed < processed_today
+  puts "What is your name?"
+    name = gets.chomp
 
-puts "What year were you born"
-year_born = gets.chomp.to_i
+  puts "How old are you?"
+    age = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread, should we order some for you?(yes/no)"
-garlic_bread = gets.chomp
+  puts "What year were you born"
+    year_born = gets.chomp.to_i
 
-puts "Would you like to enroll in the company's health insurance plan?"
-health_insurance = gets.chomp
+  puts "Our company cafeteria serves garlic bread, should we order some for you?(yes/no)"
+    garlic_bread = gets.chomp
 
-current_year = 2016.to_i
+  puts "Would you like to enroll in the company's health insurance plan?"
+    health_insurance = gets.chomp
+
+    current_year = 2016.to_i
 # If current year minus year born is equal to employee_age then correct_age is true
-if age == current_year - year_born
-  employee_age = true
-else
-  employee_age = false
-end
+  if age == current_year - year_born
+    employee_age = true
+  else
+    employee_age = false
+  end
 # p employee_age
 # If garlic bread is equal to yes then garlic_bread will be true
-if garlic_bread == "yes"
-  garlic_bread = true
-else
-  garlic_bread = false
-end
+  if garlic_bread == "yes"
+    garlic_bread = true
+  else
+    garlic_bread = false
+  end
 # p garlic_bread
 # Conditional statement for insurance plan
-if health_insurance == "yes"
-  health_insurance = true
-else
-  health_insurance = false
-end
+  if health_insurance == "yes"
+    health_insurance = true
+  else
+    health_insurance = false
+  end
 # p health_insurance
 # Conditional statement with employee age, garlic bread & insurance plan
 # to update variable as each condition is checked
 # Add results variable
+  results = "Results inconclusive"
+  if employee_age && (garlic_bread && health_insurance)
+    results = "Probably not a vampire"
+  elsif employee_age && (!garlic_bread || !health_insurance)
+    results = "Probably a vampire"
+  else !employee_age && (!garlic_bread && !health_insurance)
+    results = "Almost certainly a vampire!"
+  end
 
+  bad_name1 = "Drake Cula"
+  bad_name2 = "Tu Fang"
 
-results = "Results inconclusive"
-if employee_age && (garlic_bread && health_insurance)
-  results = "Probably not a vampire"
-elsif employee_age && (!garlic_bread || !health_insurance)
-  results = "Probably a vampire"
-else !employee_age && (!garlic_bread && !health_insurance)
-  results = "Almost certainly a vampire!"
-end
-
-bad_name1 = "Drake Cula"
-bad_name2 = "Tu Fang"
-
-if name == bad_name1 || bad_name2
-  results = "Definitley a vampire!"
+  if name = bad_name1 || bad_name2
+    results = "Definitley a vampire!"
+  end
+  employees_processed += 1
 end
