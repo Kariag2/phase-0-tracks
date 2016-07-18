b4c02c6ec96ca5f207c88226ce911c13ff3f6787
Kari Anne <kariag@icloud.com>
2016-07-17 21:51:42 -0400

    Add encrypt method that advances letter of a string one letter forward

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -5,9 +5,18 @@ Advances every letter of a string one letter forward
 Assume lowercase input and output
 Any space characters should remain spaces
 =end
-
-
-
+def encrypt(password)
+  index = 0
+  while index < password.length
+    password[index] = password[index].next
+    index += 1
+  end
+  puts password
+end
+puts encrypt('abc')
+puts encrypt('kari anne')
+puts encrypt('xyz')
+puts encrypt('wxy')
 =begin
 - Decrypt method:
 Write a decrypt method that
