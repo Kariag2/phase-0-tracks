edfbe7edbc6132059905d51418fe73f58d025c97
Kari Anne <kariag@icloud.com>
2016-07-19 10:40:24 -0400

    Add comment about ordinal number and relation to z

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -5,38 +5,39 @@
 # committed the crime, add additional calls to prove it.
 
 "iNvEsTiGaTiOn".swapcase
-# => “InVeStIgAtIoN”
+=> “InVeStIgAtIoN”
 
 "zom".insert(2, 'o')
-# => “zoom”
+=> “zoom”
 
 "enhance".center(15)
-# => "    enhance    "
+=> "    enhance    "
 
 "Stop! You’re under arrest!".upcase
-# => "STOP! YOU’RE UNDER ARREST!"
+=> "STOP! YOU’RE UNDER ARREST!"
 
 a = "suspects"
 a.prepend("the usual ")
 a
-#=> "the usual suspects"
+=> "the usual suspects"
 
 " suspects".insert(0, 'the usual')
-# => "the usual suspects"
+=> "the usual suspects"
 
 "The case of the disappearing last letter".chop
-# => "The case of the disappearing last lette"
+=> "The case of the disappearing last lette"
 
 "The mystery of the missing first letter".delete"T"
-# => "he mystery of the missing first letter"
+=> "he mystery of the missing first letter"
 
 "Elementary,    my   dear        Watson!".squeeze
-# => "Elementary, my dear Watson!"
+=> "Elementary, my dear Watson!"
 
 "z".ord
-# => 122
+=> 122
 # (What is the significance of the number 122 in relation to the character z?)
-
+# Had a hard time finding info on this I know ord is ordinal number.
+# I am curious to know more information on this
 a = "How many times does the letter 'a' appear in this string?"
 a.count "a"
-# => 4
+=> 4
