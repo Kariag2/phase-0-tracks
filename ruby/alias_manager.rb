5960f0e22245fe3983ff82ad29758efd66d4f85d
Kari Anne <kariag@icloud.com>
2016-07-19 15:10:18 -0400

    Add method that splits name and then reverses the first and last name

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,9 @@
+# write a method that takes a spy's real name (e.g., "Felicia Torres")
+# and creates a fake name with it by doing the following:
+# Swapping the first and last name
+
+def name_swap(name)
+  new_name = name.split(' ')
+  spy_name = new_name[1] + ' ' + new_name[0]
+end
+p name_swap("Kari Gilchrist")
