8a083ac75181d0831a874b8d5838d8aa435c25c3
Kari Anne <kariag@icloud.com>
2016-07-20 12:18:25 -0400

    Add stage 3 name of stage and band info

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -15,6 +15,16 @@ riot_fest = {
       'De La Soul'
     ]
   },
+  stage_3: {
+    stage_name: 'Rock',
+    band_info: [
+      'Rancid',
+      'Iggy Pop',
+      'Billy Idol'
+    ]
+  }
 }
 # p riot_fest[:stage_1][:band_info][2]
 # expected output 'Death'
+#p riot_fest[:stage_2][:stage_name]
+#expected output 'Riot'
