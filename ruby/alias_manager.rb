5c73e4a93d9bcd1cddba5cd57e248c2465b7b5f0
Kari Anne <kariag@icloud.com>
2016-07-22 19:07:05 -0400

    Add conditional statements to name swap method so to change the vowels and the consonants in our name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,30 +2,48 @@
 # and creates a fake name with it by doing the following:
 # Swapping the first and last name
 
-# def name_swap(name)
-#   new_name = name.split(' ')
-#   spy_name = new_name[1] + ' ' + new_name[0]
-#
-# end
-# #p name_swap("Kari Gilchrist")
-#
-# # Change all of the vowels to the next vowel in 'aeiou'
-# def change_vowels(vowel)
-#   vowels = 'aeioua'
-#   new_vowel = vowels.index(vowel)
-#   next_vowel = new_vowel + 1
-#   swapped_vowel = vowels[next_vowel]
-# end
-# p change_vowels("a")
-# p change_vowels("e")
-# p change_vowels("i")
-# p change_vowels("o")
-# p change_vowels("u")
-# Change all consonants to the next consonant exculding all vowels
+def change_vowels(vowel)
+  vowels = 'aeioua'
+  new_vowel = vowels.index(vowel)
+  next_vowel = new_vowel + 1
+  swapped_vowel = vowels[next_vowel]
+end
+
 def change_consonant(consonant)
   consonants = 'bcdfghjklmnpqrstvwxyzb'
   new_consonants = consonants.index(consonant)
   next_consonant = new_consonants + 1
   swapped_consonant = consonants[next_consonant]
 end
+
+def name_swap(name)
+  new_name = name.downcase.split(' ')
+  spy_name = new_name[1] + ' ' + new_name[0]
+  vowels = 'aeioua'
+  consonants = 'bcdfghjklmnpqrstvwxyzb'
+  name_change = ""
+  spy_name.chars.each do |letter|
+    if vowels.include?(letter)
+      new_letter = change_vowels(letter)
+      p new_letter
+    elsif consonants.include?(letter)
+      new_letter = change_consonant(letter)
+      p new_letter
+    end
+  end
+  change_name = name_change.split(' ')
+end
+p name_swap("Kari Gilchrist")
+
+# Change all of the vowels to the next vowel in 'aeiou'
+
+p change_vowels("a")
+p change_vowels("e")
+p change_vowels("i")
+p change_vowels("o")
+p change_vowels("u")
+
+# Change all consonants to the next consonant exculding all vowels
+
 p change_consonant("z")
+p change_consonant('d')
