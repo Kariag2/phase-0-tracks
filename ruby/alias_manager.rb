278e3561d8579a7806e0140761edcc110325cda1
Kari Anne <kariag@icloud.com>
2016-07-22 15:00:54 -0400

    Add method that will change each consonant to the next consonant excluding all vowels

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,12 +2,12 @@
 # and creates a fake name with it by doing the following:
 # Swapping the first and last name
 
-# def name_swap(name)
-#   new_name = name.split(' ')
-#   spy_name = new_name[1] + ' ' + new_name[0]
-#
-# end
-# p name_swap("Kari Gilchrist")
+def name_swap(name)
+  new_name = name.split(' ')
+  spy_name = new_name[1] + ' ' + new_name[0]
+
+end
+#p name_swap("Kari Gilchrist")
 
 # Change all of the vowels to the next vowel in 'aeiou'
 def change_vowels(vowel)
@@ -16,8 +16,11 @@ def change_vowels(vowel)
   next_vowel = new_vowel + 1
   swapped_vowel = vowels[next_vowel]
 end
-p change_vowels("a")
-p change_vowels("e")
-p change_vowels("i")
-p change_vowels("o")
-p change_vowels("u")
+# p change_vowels("a")
+# p change_vowels("e")
+# p change_vowels("i")
+# p change_vowels("o")
+# p change_vowels("u")
+def change_consonant(consonant)
+  consonants = 'bcdfghjklmnpqrstvwxyzb'
+end
