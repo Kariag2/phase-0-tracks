7dafa00b2c8befd0674174b04ead4fc05dd06a06
Kari Anne <kariag@icloud.com>
2016-07-22 23:15:02 -0400

    Add refigure fibonacci adder so it does not print 100 arrays and add driver code to see if it equalled the correct number

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,40 +1,53 @@
-arr = [42, 89, 23, 1]
-#def search_array(arr,number)
-#  arr.index(number)
-#end
-def search_array(arr, number)
-  index = 0
-  arr.each do |i|
-      if i == number
-        puts index
-        break
-      end
-      index += 1
-    end
-end
-p search_array(arr, 23)
+# arr = [42, 89, 23, 1]
+# #def search_array(arr,number)
+# #  arr.index(number)
+# #end
+# def search_array(arr, number)
+#   index = 0
+#   arr.each do |i|
+#       if i == number
+#         puts index
+#         break
+#       end
+#       index += 1
+#     end
+# end
+# p search_array(arr, 23)
 #p search_array(arr, 48)
 #p search_array(arr, 1)
 #p search_array(arr, 89)
 #p search_array(arr, 48)
 #p search_array(arr, 1)
 
-# def fibonacci_adder(x)
-#   array = [0,1,1,2,3,5]
-#   if x >= 7
-#     (x-6).times do
-#       array << (array[-1] + array[-2])
-#       p array
-#     end
-#   else
-#     p array[0...x]
-#   end
-# end
-#
-# fibonacci_adder(100)
-#
+def fibonacci_adder(x)
+  array = [0,1]
+    if x > 2
+      (x-2).times do
+        array << (array[-1] + array[-2])
+      end
+    else
+        array
+    end
+array
+end
+# p fibonacci_adder(100)
+p fibonacci_adder(100).last == 218922995834555169026
+
+#    if x > 2
+#      (x-2).times do
+#        array << (array[-1] + array[-2])
+#      end
+#    else
+#        array[0,1]
+#  end
+#p array
+#end
+
+#p fibonacci_adder(3)
+
+
 # array = [4 ,32, 567, 1, -54]
-#
+
 # #Bubble Sort
 # #Define a variable called "swapped", which is true if no swaps were made
 # #Repeatedly iterate through the array until no swaps need to be made
