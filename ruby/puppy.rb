70cf377b0d589ab87f95978b2457695898590caa
Kari Anne <kariag@icloud.com>
2016-07-26 10:41:50 -0400

    Add characteristics and behaviors to a puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+SPECIES ----------------------------
+Dog
+
+CHARACTERISTICS --------------------
+Name: german sheperd
+Color: brown and black
+Spots: none
+Leg count: 4
+Eye count: 2
+Friendly: mostly
+Tongue Length: long
+
+
+BEHAVIOR ------------------------------
+Jumps
+Digs
+Plays Fetch
+Chases Ball
+Catches Frisbees
+Sits
+Shakes
+Rolls Over
