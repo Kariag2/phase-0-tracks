c29ca3efbf4a38eba13d15c69ef88c8879318f17
Kari Anne <kariag@icloud.com>
2016-07-26 19:41:51 -0400

    Add eat milk and cookies method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,14 @@
 # Make a santa class
 class Santa
 # Input: A speak method for Santa
-# Output: A method that will print "Ho, ho, ho! Haaaappy holidays!"
+# Output: Print "Ho, ho, ho! Haaaappy holidays!"
    def speak
      puts "Ho, ho, ho! Haaaappy holidays!"
+   end
+# Input: A method with a parameter to take a cookie type
+# Output: Print that was a good type of cookie
+   def eat_milk_and_cookies(cookie)
+     puts "That was a good #{cookie}"
    end
 
 end
