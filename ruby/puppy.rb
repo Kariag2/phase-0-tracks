e4f6c8775ff089e8c6b698cc93256318bfb42965
Kari Anne <kariag@icloud.com>
2016-07-26 10:53:59 -0400

    Comment out puppy file

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,23 +1,23 @@
-SPECIES ----------------------------
-Puppy
+# SPECIES ----------------------------
+# Puppy
 
-CHARACTERISTICS --------------------
-Name: german sheperd
-Color: brown and black
-Spots: none
-Leg count: 4
-Eye count: 2
-Ear count: 2
-Friendly: mostly
-Tongue Length: long
+# CHARACTERISTICS --------------------
+# Name: german sheperd
+# Color: brown and black
+# Spots: none
+# Leg count: 4
+# Eye count: 2
+# Ear count: 2
+# Friendly: mostly
+# Tongue Length: long
 
 
-BEHAVIOR ------------------------------
-Jumps
-Digs
-Plays Fetch
-Chases Ball
-Catches Frisbees
-Sits
-Shakes
-Rolls Over
+# BEHAVIOR ------------------------------
+# Jumps
+# Digs
+# Plays Fetch
+# Chases Ball
+# Catches Frisbees
+# Sits
+# Shakes
+# Rolls Over
