c36b1a338b2521174fbfa299f9de444f05094970
Kari Anne <kariag@icloud.com>
2016-07-26 19:32:30 -0400

    Add a speak method to santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,9 @@
 # Make a santa class
 class Santa
-
+# Input: A speak method for Santa
+# Output: A method that will print "Ho, ho, ho! Haaaappy holidays!"
+   def speak()
+     puts "Ho, ho, ho! Haaaappy holidays!"
+   end
    
 end
