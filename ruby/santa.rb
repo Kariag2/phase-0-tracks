f2353145c430c28153c85f890735cc3342362b6c
Kari Anne <kariag@icloud.com>
2016-07-26 19:37:10 -0400

    Initialize Santa class in driver code and print speak method to make sure it works

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,8 +2,10 @@
 class Santa
 # Input: A speak method for Santa
 # Output: A method that will print "Ho, ho, ho! Haaaappy holidays!"
-   def speak()
+   def speak
      puts "Ho, ho, ho! Haaaappy holidays!"
    end
-   
+
 end
+santa = Santa.new
+p santa.speak
