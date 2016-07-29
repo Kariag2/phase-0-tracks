0672b13b2b1e0390a1312ef83988fa36ac4d2c7f
Kari Anne <kariag@icloud.com>
2016-07-28 20:52:45 -0400

    Add shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,19 @@
+# module Shout
+#   def self.yell_angrily(words)
+#       words + "!!!" + " :("
+#     end
+#
+#     def self.yelling_happily(happy_words)
+#       happy_words + ":):):)" + "!!!!"
+#     end
+# end
+#
+# p Shout.yell_angrily('This is bullshit')
+# p Shout.yelling_happily('I love you')
+
+# Write two classes representing anything that might shout
 module Shout
-  def self.yell_angrily(words)
-      words + "!!!" + " :("
-    end
-
-    def self.yelling_happily(happy_words)
-      happy_words + ":):):)" + "!!!!"
-    end
+  def yell(words)
+    words.upcase + "!!!!"
+  end
 end
-
-p Shout.yell_angrily('This is bullshit')
-p Shout.yelling_happily('I love you')
