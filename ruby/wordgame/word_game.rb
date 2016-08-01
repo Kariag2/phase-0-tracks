ef752eb36ee26519b8e9cc8b8660efc2c661b631
Kari Anne <kariag@icloud.com>
2016-08-01 13:59:32 -0400

    Add a method that retrieves a word from the array based on its index

--- ruby/wordgame/word_game.rb
+++ ruby/wordgame/word_game.rb
@@ -8,10 +8,15 @@ attr_accessor :array
   def get_word
     @array
   end
-  # def get_word
-  #   puts "What word would you like to use in this guessing game?"
-  #   word = gets.chomp
-  # end
+
+  def retrieve_word(i)
+    @array[i]
+  end
+
 end
 
 puts "Welcome to the Word Guessing Game!"
+# def get_word
+#   puts "What word would you like to use in this guessing game?"
+#   word = gets.chomp
+# end
