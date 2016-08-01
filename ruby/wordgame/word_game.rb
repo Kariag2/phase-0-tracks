f5ff16e9f7d80ecb59923cfca9f560a1b2d850ba
Kari Anne <kariag@icloud.com>
2016-08-01 12:40:23 -0400

    Add words to an array to use for guessing

--- ruby/wordgame/word_game.rb
+++ ruby/wordgame/word_game.rb
@@ -1,3 +1,13 @@
 class WordGame
-  
+  attr_accessor :word
+
+  def intialize
+    @word = []
+  end
+
+  # def get_word
+  #   puts "What word would you like to use in this guessing game?"
+  #   word = gets.chomp
+  # end
+
 end
