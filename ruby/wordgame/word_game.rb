44b449fa12ae20c41dc875ebbe86ec3c47add098
Kari Anne <kariag@icloud.com>
2016-08-01 12:41:38 -0400

    Add driver code to initialize the word game

--- ruby/wordgame/word_game.rb
+++ ruby/wordgame/word_game.rb
@@ -2,12 +2,13 @@ class WordGame
   attr_accessor :word
 
   def intialize
-    @word = []
+    @word = ["unicorns", "horses", "rainbows"]
   end
 
   # def get_word
   #   puts "What word would you like to use in this guessing game?"
   #   word = gets.chomp
   # end
-
 end
+
+game = WordGame.new
