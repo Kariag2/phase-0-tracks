aea98f1159315f4a11e37543a46554e7dc30328a
Kari Anne <kariag@icloud.com>
2016-08-01 14:57:44 -0400

    Add a guess a letter method

--- ruby/wordgame/word_game.rb
+++ ruby/wordgame/word_game.rb
@@ -10,12 +10,31 @@ attr_accessor :array
   end
 
   def retrieve_word(i)
+    p @array[i]
     @array[i]
   end
+
+  # Input: Based on the word given get the word length
+  # Output: Use the word length to return that many underscores
+
+  def underscore(i)
+    how_many_underscores = @array[i].length
+    underscores = '_' * how_many_underscores
+  end
+  # Input: User guesses a letter in the word
+  # Output: If the letter is in the word it replaces the representing underscore
+          # Else it uses up one guess
+  def guess_letter(x)
+    puts "Guess a letter"
+    letter = gets.chomp.downcase
+    
+  end
+
 
 end
 
 puts "Welcome to the Word Guessing Game!"
+
 # def get_word
 #   puts "What word would you like to use in this guessing game?"
 #   word = gets.chomp
