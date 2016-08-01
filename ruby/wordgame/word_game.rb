f4dbbf32828033d2f0305fe1543aacb2751cb5fd
Kari Anne <kariag@icloud.com>
2016-08-01 16:14:01 -0400

    Add a while loop and a user interface to guess a letter

--- ruby/wordgame/word_game.rb
+++ ruby/wordgame/word_game.rb
@@ -1,8 +1,11 @@
 class WordGame
-attr_accessor :array
+attr_reader :array, :letters, :is_over
+#attr_accessor :letters
 
-  def initialize(array)
+  def initialize(array,  letters)
     @array = array
+    @letters = letters
+    @is_over = false
   end
 
   def get_word
@@ -13,28 +16,36 @@ attr_accessor :array
     p @array[i]
     @array[i]
   end
-
   # Input: Based on the word given get the word length
   # Output: Use the word length to return that many underscores
-
   def underscore(i)
     how_many_underscores = @array[i].length
     underscores = '_' * how_many_underscores
   end
+
+  def guess_letter
+    puts "Guess a letter......"
+    @letters = gets.chomp.downcase
+    new_letter = @letter
+  end
   # Input: User guesses a letter in the word
   # Output: If the letter is in the word it replaces the representing underscore
           # Else it uses up one guess
-  def guess_letter(x)
-    puts "Guess a letter"
-    letter = gets.chomp.downcase
-    
-  end
+  #
+  #   puts "Guess a letter...."
+  #   @letters = gets.chomp.downcase
+  # end
 
-
-end
-
+word_game = WordGame.new("unicorns", "c")
 puts "Welcome to the Word Guessing Game!"
 
+
+while !word_game.is_over
+  puts "Guess a letter......"
+  letter_guess = gets.chomp
+end
+
+end
 # def get_word
 #   puts "What word would you like to use in this guessing game?"
 #   word = gets.chomp
