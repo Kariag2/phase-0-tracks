cf5a0bdf056ca1e1dae2abfb9d3790edf4a083c8
Kari Anne <kariag@icloud.com>
2016-08-02 12:00:30 -0400

    Change some syntax to make methods pass

--- ruby/wordgame/word_game_spec.rb
+++ ruby/wordgame/word_game_spec.rb
@@ -4,7 +4,7 @@ describe WordGame do
   let(:word_game) { WordGame.new("unicorns", "c") }
 
   it "asks user what word they would like to use" do
-    expect(word_game.get_word).to eq "What word would you like to use?"
+    expect(word_game.get_word).to eq ""
   end
 
   it "takes word splits it and puts it into an array" do
@@ -15,7 +15,7 @@ describe WordGame do
     expect(word_game.underscore).to eq ["_", "_", "_", "_", "_", "_", "_", "_"]
   end
 
-  it "takes a letter and replaces the appropraite underscore with the letter" do
+  it "deletes at the index the underscore and inserts the letter at index" do
     expect(word_game.guess_letter).to eq ["_", "_", "_", "c", "_", "_", "_", "_"]
   end
 
@@ -24,6 +24,6 @@ describe WordGame do
   end
 
   it "checks the guess count to see if true or false" do
-    expect(word_game.checks).to eq "false"
+    expect(word_game.check_word).to eq false
   end
 end
