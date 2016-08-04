4a9782a9cf8b5ffc7ecd202d4f8b980f43b68592
Kari Anne <kariag@icloud.com>
2016-08-04 17:13:25 -0400

    Add in directions for referencing and pseudocode for creating a hash

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,13 +1,13 @@
-# Declare an array of four colors
+// Declare an array of four colors
 
 var colorArray = ["Green", "Gold", "Red", "Brown"];
 
-# Declare an array of four names you would give a horse
+// Declare an array of four names you would give a horse
 
 var nameArray = ["Timmys Prize", "American Pharo", "Justice or Salt", "Just Ed"];
 
-# Add another color to the colors array
-# and another horse name to the horses array.
+ // Add another color to the colors array
+ // and another horse name to the horses array.
 console.log(colorArray);
 console.log(nameArray);
 
@@ -16,3 +16,11 @@ nameArray.push("Big Steve");
 
 console.log(colorArray);
 console.log(nameArray);
+
+// DIRECTIONS FOR REFERENCING
+// add code that will use your two arrays to create an object.
+// The keys of your object should be horse names, and the values should be colors.
+// Your solution should be something that would work for any number of colors/horses,
+// as long as the two arrays are the same length
+
+// Create a container that will have horses names as keys and colors as values
