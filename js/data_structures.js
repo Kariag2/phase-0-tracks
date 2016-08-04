4eb62d12233b5845874454ee088781d3e11554d9
Kari Anne <kariag@icloud.com>
2016-08-04 17:05:10 -0400

    Add an array of four horses names

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,7 @@
 # Declare an array of four colors
 
 var colorArray = ["Green", "Gold", "Red", "Brown"];
+
+# Declare an array of four names you would give a horse
+
+var nameArray = ["Timmys Prize", "American Pharo", "Justice or Salt", "Just Ed"];
