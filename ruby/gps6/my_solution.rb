7894e0fea22b99c86b1d020ecd3e9510a7f09328
Kari Anne <kariag@icloud.com>
2016-08-03 21:44:22 -0400

    Add in refactored methods worked on with Rusty

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,7 +1,7 @@
 # Virus Predictor
-require "pry"
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# require "pry"
+# I worked on this challenge [by myself, with: Russell Hutto].
+# We spent [1.5] hours on this challenge.
 
 # EXPLANATION OF require_relative
 # Require_relative links the state data file because it is in the same directory. It compliments the built in method require. It is a shortcut to require in pulling in files
@@ -20,60 +20,58 @@ class VirusPredictor
 
 # Predicted death method and speed of spread method
 # it runs both methods at the same time
-
+  # def virus_effects
+  #   predicted_deaths
+  #   speed_of_spread
+  # end
+# Refactored
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+   print "#{@state} will lose #{predicted_deaths} print people in this outbreak"
+   puts " and will spread across the state in #{speed_of_spread} months.\n\n"
   end
-
   private
 
 # Takes three arguments population density population and state
 # Predicts the number of deaths based on the population
 # Uses the floor method to round down from the float, no decimal
 
-  def predicted_deaths(population_density, population, state)
-    # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
-    end
-
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
+# Refactored
+  def predicted_deaths
+   # predicted deaths is solely based on population density
+      if @population_density >= 200
+        (@population * 0.4).floor
+      elsif @population_density >= 150
+        (@population * 0.3).floor
+      elsif @population_density >= 100
+        (@population * 0.2).floor
+      elsif @population_density >= 50
+        (@population * 0.1).floor
+      else
+        (@population * 0.05).floor
+      end
   end
 # Take arguments population density and state and calculates the speed at which the virus will spread
 
-  def speed_of_spread(population_density, state) #in months
-    # We are still perfecting our formula here. The speed is also affected
-    # by additional factors we haven't added into this functionality.
-    speed = 0.0
+# Refactored
+def speed_of_spread #in months
+   # We are still perfecting our formula here. The speed is also affected
+   # by additional factors we haven't added into this functionality.
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
-    end
+   if @population_density >= 200
+     0.5
+   elsif @population_density >= 150
+     1
+   elsif @population_density >= 100
+     1.5
+   elsif @population_density >= 50
+     2
+   else
+     2.5
+   end
 
-    puts " and will spread across the state in #{speed} months.\n\n"
-
-  end
+ end
 
 end
-
 #=======================================================================
 
 # DRIVER CODE
