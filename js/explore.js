e8ec01708101e1e16c2ae54a31a76e5de984b6be
Kari Anne <kariag@icloud.com>
2016-08-04 13:07:30 -0400

    Add directions into code as pseudocode just for referencing

--- js/explore.js
+++ js/explore.js
@@ -1,6 +1,16 @@
 // pseudocode a function that takes a string as a parameter and reverses the string.
+// reverse("hello"), for example, should return "olleh".
+//This isn't a task specific to JavaScript -- your pseudocode should be in plain
+//English and use phrases like "for each" instead of JavaScript-specific terms.
+//Remember that you can add strings in JavaScript, so '' + 'a' would result in
+//the string 'a'. Take your time. This is the most valuable skill in programming:
+//being able to think through a problem logically.
+
+
 // Write a function that takes a word and reverses the word
 
-function wordReverse(word){
-  
+function wordReverse(){
+  return word.reverse;
 }
+
+console.log(wordReverse("Hello"));
