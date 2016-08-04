50e4a1fed8d0e1d121e43702d3f2025b32028daf
Kari Anne <kariag@icloud.com>
2016-08-04 17:07:42 -0400

    Add a color to color array and horse name to name array

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,9 @@ var colorArray = ["Green", "Gold", "Red", "Brown"];
 # Declare an array of four names you would give a horse
 
 var nameArray = ["Timmys Prize", "American Pharo", "Justice or Salt", "Just Ed"];
+
+# Add another color to the colors array
+# and another horse name to the horses array.
+
+colorArray.push("Yellow");
+nameArray.push("Big Steve");
