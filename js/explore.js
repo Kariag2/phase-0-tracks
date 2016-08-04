85d9d0af3cd5a901f876cf36ae1265972639915e
Kari Anne <kariag@icloud.com>
2016-08-04 14:37:25 -0400

    Pseudocode to break down the way to reverse a string into smaller parts

--- js/explore.js
+++ js/explore.js
@@ -11,6 +11,7 @@
 // Use split method on a string to split it into an array
 // So you can call the reverse method on the array
 // Then join them back together with the join method
+// Then store the reversed word or phase in a new variable
 
 // function reverse(word){
 //     return word.split("").reverse().join("");
@@ -30,8 +31,11 @@
 
 // EXPLORING DIFFERENT WAYS TO REVERSE STRINGS
 
-//Write a function that takes a word as a parameter
-//
+// Write a function that takes a string as a parameter
+// Reverse the string
+//         Use the index of the letters in the string to change the letter
+//         to its opposite using iteration
+// Store the strings in new variables
 
 function reverse(string) {
   var word = '';
