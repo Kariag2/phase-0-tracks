5da98e2eb2748773b24fef2db052c3897aa76a85
Kari Anne <kariag@icloud.com>
2016-08-04 12:44:39 -0400

    Write a word reverse function

--- js/explore.js
+++ js/explore.js
@@ -1,2 +1,6 @@
 // pseudocode a function that takes a string as a parameter and reverses the string.
 // Write a function that takes a word and reverses the word
+
+function wordReverse(word){
+  
+}
