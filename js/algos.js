ecf20dc019000ec1324cf55a3490364184e4b5dc
Kari Anne <kariag@icloud.com>
2016-08-05 12:20:39 -0400

    Add a phrase array with three different length phrases in it

---
+++ js/algos.js
@@ -0,0 +1,4 @@
+// Write a function that takes an array of words or phrases and returns the
+// longest word or phrase in the array.
+
+var phraseArray = ["A phrase", "A longer phrase", "The longest phrase expected as return"];
