2c37f036b303dfd2d17e08187b39566df83a11ce
Kari Anne <kariag@icloud.com>
2016-08-05 12:42:50 -0400

    Add a loop that loops through the phrase array right now is returning the first phrase not the longest

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,20 @@
 // Write a function that takes an array of words or phrases and returns the
 // longest word or phrase in the array.
 
+// Name an array and add three phrases with different length phrases to it
+// Print the array just for testing
+// Loop through the array to find the index of the phrase that has the longest length
+
+
 var phraseArray = ["A phrase", "A longer phrase", "The longest phrase expected as return"];
+
+console.log(phraseArray);
+
+var longestPhrase = 0
+for (var i = 0; i < phraseArray.length; i++){
+  if (longestPhrase < phraseArray[i].length){
+    longestPhrase = phraseArray[i];
+  }
+}
+
+console.log(longestPhrase)
